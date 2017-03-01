6b696e5cf8e1ce41caeaa0c371564b12749338da
Sarah Eldafrawy <sarah.eldafrawy257@gmail.com>
2017-03-01 21:11:34 +0200

    first_assignment

---
+++ data-structure/data structure/src/eg/edu/alexu/csd/datastructure/calculator/cs29/main.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.csd.datastructure.calculator.cs29;
+
+class test{
+	
+public static void main(String[] args) {
+	
+   Mycalculator xy = new Mycalculator();
+ 
+   
+   System.out.println(xy.divide(4,2));
+	
+}
+}
