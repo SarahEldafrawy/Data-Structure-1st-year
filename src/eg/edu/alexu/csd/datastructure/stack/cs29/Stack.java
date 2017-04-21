1269d51f6a867aa0809af9efdbaede490bda0c38
Sarah Eldafrawy <sarah.eldafrawy257@gmail.com>
2017-04-21 11:31:58 +0200

    stack

---
+++ src/eg/edu/alexu/csd/datastructure/stack/cs29/Stack.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+/**
+ * @author welcome
+ *
+ */
+package eg.edu.alexu.csd.datastructure.stack.cs29;
+
+import eg.edu.alexu.csd.datastructure.stack.IStack;
+/**
+ *
+ * @author welcome
+ *
+ */
+public class Stack implements IStack {
+
+	/**
+	 */
+	class Plate {
+		private Plate prev = null;
+		private Object data;
+	}
+	/**
+	 */
+	private int size = 0;
+	/**
+	 */
+	private Plate top = null;
+
+	/**
+	* Inserts a specified element at the specified position in the list.
+	* @param index zero-based index
+	* @param element object to insert
+	*/
+	@Override
+	public void add(final int index, final Object element) {
+//		Plate plateToAdd = new Plate();
+//		if (top == null) {
+//			plateToAdd.data = element;
+//			top = plateToAdd;
+//		} else {
+//			plateToAdd.data = element;
+//			plateToAdd.prev = top;
+//			top = plateToAdd;
+//			// loop for index and with temp stack
+//		}
+	}
+	/**
+	* Removes the element at the top of stack and returns that element.
+	* @return top of stack element, or through exception if empty
+	*/
+	@Override
+	public Object pop() {
+		if (top == null) {
+			throw new RuntimeException(); // throw exception
+		}
+		Object value = top.data;
+		Plate temp = top.prev;
+		if (top.prev != null) {
+			top.prev = null;
+		}
+		top = temp;
+		size--;
+		return value;
+	}
+	/**
+	* Get the element at the top of stack without removing it from stack.
+	* @return top of stack element, or through exception if empty
+	*/
+	@Override
+	public Object peek() {
+		if (top == null) {
+			throw new RuntimeException();
+		}
+		return top.data;
+	}
+	/**
+	* Pushes an item onto the top of this stack.
+	* @param element to insert
+	*/
+	@Override
+	public void push(final Object element) {
+		Plate plateToAdd = new Plate();
+		if (top == null) {
+			plateToAdd.data = element;
+			top = plateToAdd;
+		} else {
+			plateToAdd.data = element;
+			plateToAdd.prev = top;
+			top = plateToAdd;
+		}
+		size++;
+	}
+	/**.
+	* Tests if this stack is empty
+	* @return true if stack empty
+	*/
+	@Override
+	public boolean isEmpty() {
+		if (top == null) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	* Returns the number of elements in the stack.
+	* @return number of elements in the stack
+	*/
+	@Override
+	public int size() {
+		return size;
+	}
+}
