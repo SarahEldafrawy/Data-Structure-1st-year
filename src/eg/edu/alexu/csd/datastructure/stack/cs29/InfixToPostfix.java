f5a2d3898e0538d381fecbadc85a854eaa291582
Sarah Eldafrawy <sarah.eldafrawy257@gmail.com>
2017-04-25 10:00:50 +0200

    infix

--- src/eg/edu/alexu/csd/datastructure/stack/cs29/InfixToPostfix.java
+++ src/eg/edu/alexu/csd/datastructure/stack/cs29/InfixToPostfix.java
@@ -9,14 +9,19 @@ import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
 public class InfixToPostfix implements IExpressionEvaluator {
 	
 	/**
-	 */
+	* Takes a symbolic/numeric infix expression as input and converts it to
+	* postfix notation. There is no assumption on spaces between terms or the
+	* length of the term (e.g., two digits symbolic or numeric term)
+	* @param expression infix expression
+	* @return postfix expression
+	*/
 	@Override
-	public String infixToPostfix(final String expresion) {
-		String expression = expresion.replaceAll("\\ ", "");
+	public String infixToPostfix(final String expression) {
+		//String expression = expresion.replaceAll("\\ ", "");
 		if (expression == "") {
 			throw new RuntimeException();
 		}
-		String str = " ";
+		String str = "";
 		Stack operator = new Stack();
 		int i = 0;
 		while (i < expression.length()) {
@@ -28,7 +33,7 @@ public class InfixToPostfix implements IExpressionEvaluator {
 				operator.push(expression.charAt(i));
 				i++;
 				while (i < expression.length() && expression.charAt(i) != '+' && expression.charAt(i) != '-' && expression.charAt(i) != '*' && expression.charAt(i) != '/' && expression.charAt(i) !='(' && expression.charAt(i) !=')') {
-					str += expression.charAt(i) + " ";
+					str += expression.charAt(i);
 					i++;
 				}
 			}
@@ -36,7 +41,7 @@ public class InfixToPostfix implements IExpressionEvaluator {
 				operator.push(expression.charAt(i));
 				i++;
 				while (i < expression.length() && expression.charAt(i) != '+' && expression.charAt(i) != '-' && expression.charAt(i) != '*' && expression.charAt(i) != '/' && expression.charAt(i) !='(' && expression.charAt(i) !=')') {
-					str += expression.charAt(i) + " ";
+					str += expression.charAt(i);
 					i++;
 				}
 			}
@@ -44,28 +49,28 @@ public class InfixToPostfix implements IExpressionEvaluator {
 					operator.push(expression.charAt(i));
 					i++;
 					while (i < expression.length() && expression.charAt(i) != '+' && expression.charAt(i) != '-' && expression.charAt(i) != '*' && expression.charAt(i) != '/' && expression.charAt(i) !='(' && expression.charAt(i) !=')') {
-						str += expression.charAt(i) + " ";
+						str += expression.charAt(i);
 						i++;
 					}
 			}
 			if (i < expression.length() && expression.charAt(i) == ')') {
 				i++;
-				while (operator.peek() != (Object)'(') {
-					str += operator.pop() + " ";
+				while (operator.peek() != (Object) '(') {
+					str += operator.pop();
 				}
 				operator.pop();
 			}
 			while (!operator.isEmpty() && operator.peek() != (Object)'(') {
-				str += operator.pop() + " ";
+				str += operator.pop();
 			}
-		}
-		if (str.length() > 1) {
-			str = str.trim();
 		}
 		return str;
 	}
 	/**
-	 */
+	* Evaluate a postfix numeric expression, with a single space separator
+	* @param expression postfix expression
+	* @return the expression evaluated value
+	*/
 	@Override
 	public int evaluate(final String expression) {
 		
