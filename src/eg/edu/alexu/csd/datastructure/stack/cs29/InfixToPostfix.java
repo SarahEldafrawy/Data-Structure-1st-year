86975e43ba36a7a9c34a5d6840d3c03082ff0d80
Sarah Eldafrawy <sarah.eldafrawy257@gmail.com>
2017-04-26 17:30:30 +0200

    evaluate cast

--- src/eg/edu/alexu/csd/datastructure/stack/cs29/InfixToPostfix.java
+++ src/eg/edu/alexu/csd/datastructure/stack/cs29/InfixToPostfix.java
@@ -7,11 +7,11 @@ import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
 /**
  */
 public class InfixToPostfix implements IExpressionEvaluator {
-	
+
 	/**
 	* Takes a symbolic/numeric infix expression as input and converts it to
-	* postfix notation. There is no assumption on spaces between terms or the
-	* length of the term (e.g., two digits symbolic or numeric term)
+	* postfix notation. There is no assumption on spaces between terms
+	* or the length of the term (e.g., two digits symbolic or numeric term)
 	* @param expression infix expression
 	* @return postfix expression
 	*/
@@ -23,82 +23,112 @@ public class InfixToPostfix implements IExpressionEvaluator {
 		}
 		int i = 0, t = 0;
 		while (i < expresion.length()) {
-			if (i < expression.length() && expression.charAt(i) == '(') {
-				t++;
-			} else if (i < expression.length() && expression.charAt(i) == ')') {
-				t--;
-			}
-			i++;
+		if (i < expression.length() && expression.charAt(i) == '(') {
+			t++;
+	} else if (i < expression.length() && expression.charAt(i) == ')') {
+			t--;
+		}
+		i++;
 		}
 		if (t != 0) {
 			throw new RuntimeException();
 		}
 		i = 0;
 		while (i < expresion.length()) {
-			if (i < expression.length() && (expression.charAt(i) == '+' || expression.charAt(i) == '-'
-					|| expression.charAt(i) == '*' || expression.charAt(i) == '/')) {
+			if (i < expression.length()
+			&& (expression.charAt(i) == '+'
+			|| expression.charAt(i) == '-'
+			|| expression.charAt(i) == '*'
+			|| expression.charAt(i) == '/')) {
 					i++;
-					if (i < expression.length() && (expression.charAt(i) == '+' || expression.charAt(i) == '-' 
-						|| expression.charAt(i) == '*' || expression.charAt(i) == '/')) {
+				if (i < expression.length()
+					&& (expression.charAt(i) == '+'
+					|| expression.charAt(i) == '-'
+					|| expression.charAt(i) == '*'
+					|| expression.charAt(i) == '/')) {
 						throw new RuntimeException();
 					}
 			}
 		i++;
 		}
 		i = expression.length() - 1;
-		if (expression.charAt(i) == '+' || expression.charAt(i) == '-' 
-				|| expression.charAt(i) == '*' || expression.charAt(i) == '/') {
+		if (expression.charAt(i) == '+'
+			|| expression.charAt(i) == '-'
+			|| expression.charAt(i) == '*'
+			|| expression.charAt(i) == '/') {
 			throw new RuntimeException();
 		}
 		String str = "";
 		Stack operator = new Stack();
 		i = 0;
 		while (i < expression.length()) {
-			while (i < expression.length() && expression.charAt(i) != '+'
-				&& expression.charAt(i) != '-' && expression.charAt(i) != '*'
-				&& expression.charAt(i) != '/' && expression.charAt(i) != '('
+			while (i < expression.length()
+				&& expression.charAt(i) != '+'
+				&& expression.charAt(i) != '-'
+				&& expression.charAt(i) != '*'
+				&& expression.charAt(i) != '/'
+				&& expression.charAt(i) != '('
 				&& expression.charAt(i) != ')') {
 				str += expression.charAt(i) + " ";
 				i++;
 			}
-			if (i < expression.length() && (expression.charAt(i) == '+' || expression.charAt(i) == '-')) {
+			if (i < expression.length()
+				&& (expression.charAt(i) == '+'
+				|| expression.charAt(i) == '-')) {
 				operator.push(expression.charAt(i));
 				i++;
-				while (i < expression.length() && expression.charAt(i) != '+'
-					&& expression.charAt(i) != '-' && expression.charAt(i) != '*'
-					&& expression.charAt(i) != '/' && expression.charAt(i) != '('
+				while (i < expression.length()
+					&& expression.charAt(i) != '+'
+					&& expression.charAt(i) != '-'
+					&& expression.charAt(i) != '*'
+					&& expression.charAt(i) != '/'
+					&& expression.charAt(i) != '('
 					&& expression.charAt(i) != ')') {
 					str += expression.charAt(i) + " ";
 					i++;
 				}
 			}
-			if (i < expression.length() && (expression.charAt(i) == '*' || expression.charAt(i) == '/')) {
+			if (i < expression.length()
+				&& (expression.charAt(i) == '*'
+				|| expression.charAt(i) == '/')) {
 				operator.push(expression.charAt(i));
 				i++;
-				while (i < expression.length() && expression.charAt(i) != '+' && expression.charAt(i) != '-' && expression.charAt(i) != '*' && expression.charAt(i) != '/' && expression.charAt(i) !='(' && expression.charAt(i) !=')') {
+				while (i < expression.length()
+						&& expression.charAt(i) != '+'
+						&& expression.charAt(i) != '-'
+						&& expression.charAt(i) != '*'
+						&& expression.charAt(i) != '/'
+						&& expression.charAt(i) != '('
+					&& expression.charAt(i) != ')') {
 					str += expression.charAt(i) + " ";
 					i++;
 				}
 			}
-			if (i < expression.length() && expression.charAt(i) == '(') {
+		if (i < expression.length() && expression.charAt(i) == '(') {
 					operator.push(expression.charAt(i));
 					i++;
-					while (i < expression.length() && expression.charAt(i) != '+'
-						&& expression.charAt(i) != '-' && expression.charAt(i) != '*' 
-						&& expression.charAt(i) != '/' && expression.charAt(i) != '(' 
-						&& expression.charAt(i) != ')') {
-						str += expression.charAt(i) + " ";
+					while (i < expression.length()
+						&& expression.charAt(i) != '+'
+						&& expression.charAt(i) != '-'
+						&& expression.charAt(i) != '*'
+						&& expression.charAt(i) != '/'
+						&& expression.charAt(i) != '('
+					&& expression.charAt(i) != ')') {
+					str += expression.charAt(i) + " ";
 						i++;
 					}
 			}
-			if (i < expression.length() && expression.charAt(i) == ')') {
+			if (i < expression.length()
+				&& expression.charAt(i) == ')') {
 				i++;
-				while (operator.peek() != (Object) '(' && !operator.isEmpty()) {
+				while (operator.peek() != (Object) '('
+						&& !operator.isEmpty()) {
 					str += operator.pop() + " ";
 				}
 				operator.pop();
 			}
-			while (!operator.isEmpty() && operator.peek() != (Object)'(') {
+			while (!operator.isEmpty()
+					&& operator.peek() != (Object) '(') {
 				str += operator.pop() + " ";
 			}
 		}
@@ -119,6 +149,7 @@ public class InfixToPostfix implements IExpressionEvaluator {
 			throw new RuntimeException();
 		}
 		int i = 0;
+		//suppose its working
 		while (i < expression.length()) {
 			if (i < expression.length() && (expression.charAt(i) == '+' || expression.charAt(i) == '-' 
 					|| expression.charAt(i) == '*' || expression.charAt(i) == '/')) {
@@ -128,48 +159,46 @@ public class InfixToPostfix implements IExpressionEvaluator {
 			}
 			i++;
 		}
-//		int operator1, operator2;
-//		int res = 0;
-//		Stack operator = new Stack();
-//		i = 0;
-//		while (i < expression.length()) {
-//			if (expression.charAt(i) == '+') {
-//				operator1 = (float) operator.pop();
-//				operator2 = (float) operator.pop();
-//				//res = operator1 + operator2;
-//				operator.push(res);
-//				i++;
-//			} else if (expression.charAt(i) == '-') {
-//				operator1 = (float) operator.pop();
-//				operator2 = (float) operator.pop();
-//				//res = operator2 - operator1;
-//				operator.push(res);
-//				i++;
-//			} else if (expression.charAt(i) == '*') {
-//				operator1 = (float) operator.pop();
-//				operator2 = (float) operator.pop();
-//				//res = operator1 * operator2;
-//				operator.push(res);
-//				i++;
-//			}
-//		if (expression.charAt(i) == '/') {
-//				operator1 = (Integer) operator.peek();
-//				operator.pop();
-//				operator2 = (Integer) operator.pop();
-//				res = operator2 + operator1;
-//				operator.push(res);
-//				i++;
-//			} else {
-//				while (i < expression.length() && expression.charAt(i) != '+'
-//					&& expression.charAt(i) != '-' && expression.charAt(i) != '*'
-//					&& expression.charAt(i) != '/') {
-//					if (expression.charAt(i) != ' ') {
-//						operator.push(expression.charAt(i));
-//					}
-//					i++;
-//				}
-//			}
-//		}
-		return 0;
+		Integer operator1, operator2;
+		int res = 0;
+		Stack operator = new Stack();
+		i = 0;
+		while (i < expression.length()) {
+			if (expression.charAt(i) == '+') {
+				operator1 = (Integer) operator.pop();
+				operator2 = (Integer) operator.pop();
+				res = operator2 + operator1;
+				operator.push(res);
+				i++;
+			} else if (expression.charAt(i) == '-') {
+				operator1 = (Integer) operator.pop();
+				operator2 = (Integer) operator.pop();
+				res = operator2 - operator1;
+				operator.push(res);
+				i++;
+			} else if (expression.charAt(i) == '*') {
+				operator1 = (Integer) operator.pop();
+				operator2 = (Integer) operator.pop();
+				res = operator2 * operator1;
+				operator.push(res);
+				i++;
+			} else if (expression.charAt(i) == '/') {
+				operator1 = (Integer) operator.pop();
+				operator2 = (Integer) operator.pop();
+				res = operator2 / operator1;
+				operator.push(res);
+				i++;
+			} else {
+				while (i < expression.length() && expression.charAt(i) != '+'
+					&& expression.charAt(i) != '-' && expression.charAt(i) != '*'
+					&& expression.charAt(i) != '/') {
+					if (expression.charAt(i) != ' ') {
+						operator.push(Character.getNumericValue(expression.charAt(i)));
+					}
+					i++;
+				}
+			}
+		}
+		return res;
 	}
 }
